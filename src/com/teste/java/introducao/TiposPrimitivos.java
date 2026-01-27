18254ffa8c243a99a3a6c02968a79d71cab5ccff
JoaoPe459 <joaope459@gmail.com>
2026-01-27 10:04:46 -0300

    Aula11

--- src/com/teste/java/introducao/TiposPrimitivos.java
+++ src/com/teste/java/introducao/TiposPrimitivos.java
@@ -4,8 +4,21 @@ public class TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, boolean, byte, byte, short
         int idade = 10;
+        long numeroGrande = 1000000;
+        double salarioDouble = 2000;
+        float salarioFloat = 3000;
+        char caracter = 'a';
+        short idadeShort = 100;
+        boolean ligado = true;
+        byte idadeByte = 100;
+        char caracter1 = '\u0041';//Unicode table
+        char caracter2 = 87;//ASCII table
+
         System.out.println(idade);
         System.out.println("idade");
         System.out.println("A idade é "+idade+"anos");
+        System.out.println("char "+caracter);
+        System.out.println("char "+caracter1);
+        System.out.println("char "+caracter2);
     }
 }
