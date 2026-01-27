4c146ede4a800c2e392ba9e7947e7b522090a324
JoaoPe459 <joaope459@gmail.com>
2026-01-27 09:50:19 -0300

    Aula10

---
+++ src/com/teste/java/introducao/TiposPrimitivos.java
@@ -0,0 +1,11 @@
+package com.teste.java.introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, boolean, byte, byte, short
+        int idade = 10;
+        System.out.println(idade);
+        System.out.println("idade");
+        System.out.println("A idade é "+idade+"anos");
+    }
+}
