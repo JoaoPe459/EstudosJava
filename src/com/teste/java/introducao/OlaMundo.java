66574fce2e53ba93a97a294619884402005e951d
JoaoPe459 <joaope459@gmail.com>
2026-01-27 09:42:10 -0300

    Aula09

--- src/com/teste/java/introducao/OlaMundo.java
+++ src/com/teste/java/introducao/OlaMundo.java
@@ -1,6 +1,14 @@
 package com.teste.java.introducao;
 
+/**
+ * Isso é um comentario <b>javadox</b>
+ */
 public class OlaMundo {
+    //Comentario em linha
+    /*
+    Comentario em
+    multiplas linhas
+     */
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
     }
