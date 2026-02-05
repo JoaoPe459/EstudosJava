ba40dda73791215a349c630075a06b9b99e412ff
JoaoPe459 <joaope459@gmail.com>
2026-02-05 15:27:50 -0300

    Aula14

---
+++ src/com/teste/java/introducao/TiposPrimitivosExecicios.java
@@ -0,0 +1,12 @@
+package com.teste.java.introducao;
+
+public class TiposPrimitivosExecicios {
+    public static void main(String[] args) {
+        String nome = "Fulano";
+        String endereco = "Rua Joaquim";
+        double salario = 1200.00;
+        String data = "05/02/2026";
+        String relatorio = "Eu, " + nome + " moro no endereço " + endereco + " confirmo que recebi o salario de " + salario + ", nada data " + data;
+        System.out.println(relatorio);
+    }
+}
