9a8e5213e332179b26e04bd5f75b14e4815add7d
JoaoPe459 <joaope459@gmail.com>
2026-02-05 19:14:18 -0300

    aula17

--- src/com/teste/java/introducao/Operadores.java
+++ src/com/teste/java/introducao/Operadores.java
@@ -9,5 +9,20 @@ public class Operadores {
         int resultado3 = num1 * num2;
         int resultado4 = num2 / num1;
         System.out.println(resultado4);
+
+        //%
+        int resto = 21%2;
+        System.out.println(resto);
+
+        //< > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualQueVinte = 10 == 20;
+        boolean isDezDiferenteQueVinte = 10 != 20;
+        System.out.println(isDezMaiorQueVinte);
+        System.out.println(isDezMenorQueVinte);
+        System.out.println(isDezIgualQueVinte);
+        System.out.println(isDezDiferenteQueVinte);
+
     }
 }
