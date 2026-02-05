75591bae1ed361640b3d17156861d3f420f92753
JoaoPe459 <joaope459@gmail.com>
2026-02-05 16:09:39 -0300

    aula15

---
+++ src/com/teste/java/introducao/Operadores.java
@@ -0,0 +1,13 @@
+package com.teste.java.introducao;
+
+public class Operadores {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int resultado1 = num1 + num2;
+        int resultado2 = num1 - num2;
+        int resultado3 = num1 * num2;
+        int resultado4 = num2 / num1;
+        System.out.println(resultado4);
+    }
+}
