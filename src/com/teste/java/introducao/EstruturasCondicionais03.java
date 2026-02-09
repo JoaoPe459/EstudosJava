6baa97f2188e753496ce62f7288fa2d82c71a2e9
JoaoPe459 <joaope459@gmail.com>
2026-02-09 11:52:51 -0300

    aula22

---
+++ src/com/teste/java/introducao/EstruturasCondicionais03.java
@@ -0,0 +1,19 @@
+package com.teste.java.introducao;
+
+public class EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        double salario = 6000;
+        String mensagemDoar = "Eu vou doar";
+        String mensagemNaoDoar = "Eu não vou doar";
+        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
+
+        /*if (salario > 5000) {
+            resultado = mensagemDoar;
+        } else {
+            resultado = mensagemNaoDoar;
+        }
+        System.out.println(resultado);
+        */
+
+    }
+}
