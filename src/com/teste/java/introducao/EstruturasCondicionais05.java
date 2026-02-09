0db183f0b3b6713d77c082c65210a261266ed3e4
JoaoPe459 <joaope459@gmail.com>
2026-02-09 12:11:30 -0300

    aula25

---
+++ src/com/teste/java/introducao/EstruturasCondicionais05.java
@@ -0,0 +1,46 @@
+package com.teste.java.introducao;
+
+public class EstruturasCondicionais05 {
+    public static void main(String[] args) {
+        byte dia = 5;
+        switch (dia) {
+            case 1:
+                System.out.println("Dom");
+                break;
+            case 2:
+                System.out.println("Seg");
+                break;
+            case 3:
+                System.out.println("Ter");
+                break;
+            case 4:
+                System.out.println("Qua");
+                break;
+            case 5:
+                System.out.println("Qui");
+                break;
+            case 6:
+                System.out.println("Sex");
+                break;
+            case 7:
+                System.out.println("Sab");
+                break;
+            default:
+                System.out.println("Opção Invalida");
+                break;
+        }
+
+        char sexo = 'M';
+        switch (sexo) {
+            case 'M':
+                System.out.println("Masculino");
+                break;
+            case 'F':
+                System.out.println("Feminino");
+                break;
+            default:
+                System.out.println("Invalido");
+
+        }
+    }
+}
