89a79e2f70ba6d4bbda3990459497e10dec65eeb
JoaoPe459 <joaope459@gmail.com>
2026-02-10 11:31:56 -0300

    aula29

---
+++ src/com/teste/java/introducao/EstruturasDeRepeticao03.java
@@ -0,0 +1,14 @@
+package com.teste.java.introducao;
+
+public class EstruturasDeRepeticao03 {
+    public static void main(String[] args) {
+        int valorMax = 50;
+        for (int i = 1; i <= valorMax; i++) {
+            if ( i <=25) {
+                System.out.println(i);
+            } else {
+                break;
+            }
+        }
+    }
+}
