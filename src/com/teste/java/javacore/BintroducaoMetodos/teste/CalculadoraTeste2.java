cfd6383d97d67b8b6ead00409ec69ee0c2ed1813
JoaoPe459 <joaope459@gmail.com>
2026-03-28 19:19:42 -0300

    aula45

---
+++ src/com/teste/java/javacore/BintroducaoMetodos/teste/CalculadoraTeste2.java
@@ -0,0 +1,10 @@
+package com.teste.java.javacore.BintroducaoMetodos.teste;
+
+import com.teste.java.javacore.BintroducaoMetodos.dominio.Calculadora;
+
+public class CalculadoraTeste2 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(10, 20);
+    }
+}
