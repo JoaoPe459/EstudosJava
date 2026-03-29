36adde0c7d792d85b0e2ea8ef6f0181910a913fe
JoaoPe459 <joaope459@gmail.com>
2026-03-28 21:39:36 -0300

    aula46

---
+++ src/com/teste/java/javacore/BintroducaoMetodos/teste/CalculadoraTeste3.java
@@ -0,0 +1,10 @@
+package com.teste.java.javacore.BintroducaoMetodos.teste;
+
+import com.teste.java.javacore.BintroducaoMetodos.dominio.Calculadora;
+
+public class CalculadoraTeste3 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        System.out.println(calculadora.divisaoDoisNumeros(20, 10));
+    }
+}
