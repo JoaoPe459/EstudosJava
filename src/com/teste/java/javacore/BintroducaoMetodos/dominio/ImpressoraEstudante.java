8616c69aada019552cd750d35bab3a7c8db0922d
JoaoPe459 <joaope459@gmail.com>
2026-04-11 18:38:20 -0300

    aula50

--- src/com/teste/java/javacore/BintroducaoMetodos/dominio/ImpressoraEstudante.java
+++ src/com/teste/java/javacore/BintroducaoMetodos/dominio/ImpressoraEstudante.java
@@ -2,6 +2,7 @@ package com.teste.java.javacore.BintroducaoMetodos.dominio;
 
 public class ImpressoraEstudante {
     public void imprime(Estudante estudante){
+        estudante.nome = "Javier";
         System.out.println(estudante.nome);
         System.out.println(estudante.idade);
         System.out.println(estudante.sexo);
